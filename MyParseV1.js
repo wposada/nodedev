9adfa14a9bdca7790e8129c69350aacc6123e1dc
wposada <wilsonposada@msn.com>
2016-02-04 16:36:01 -0500

    Update MyParseV1.js

--- MyParseV1.js
+++ MyParseV1.js
@@ -102,7 +102,7 @@ lineReader.eachLine('/media/sf_Ingress/Order/Junio/file_0630.txt', function(line
       });
 }
 
-  console.log(time+"-"+names+"-"+action+"\n");
+  console.log(time+"-"+names+"-"+dirs+"\n");
 }).then(function () {
   console.log("I'm done!!");
   connection.end();
