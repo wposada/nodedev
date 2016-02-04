50b2bc950ed483eee51a6a73625f02ef99892864
wposada <wilsonposada@msn.com>
2016-02-04 16:17:54 -0500

    Update MyParseV1.js

--- MyParseV1.js
+++ MyParseV1.js
@@ -95,12 +95,13 @@ var promise = htmlToJson.parse(html, {
 lineReader.eachLine('/media/sf_Ingress/Order/Junio/file_0630.txt', function(line) {
   Parseando(line);
   if(time !=""){
-      connection.query('INSERT INTO portals (name, lat, lng) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE name=?, lat=?,lng=?',[names,lat,lng,names,lat,lng], function(err, rows, fields) {
+      //connection.query('INSERT INTO portals (name, lat, lng) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE name=?, lat=?,lng=?',[names,lat,lng,names,lat,lng], function(err, rows, fields) {
+      connection.query('INSERT INTO portals (name, lat, lng) VALUES (?, ?, ?)',[names,lat,lng], function(err, rows, fields) {
          if (err) throw err;
       });
 }
 
-  console.log(time+"-"+names+"\n");
+  console.log(time+"-"+names+"-"+action+"\n");
 }).then(function () {
   console.log("I'm done!!");
   connection.end();
