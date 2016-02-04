680da284db4bbeb9b4daa31de32d42e4aadb0dff
wposada <wilsonposada@msn.com>
2016-02-04 14:32:51 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-   host: 'localhost',
+   host: '127.0.0.1',
    user: 'root',
    password: 'bitnami',
    database: 'cake',
