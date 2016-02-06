3d0681a129eef1d25aa5676fb3536e28e81859f4
wposada <wilsonposada@msn.com>
2016-02-06 18:49:32 -0500

    Update MyParseV1.js

--- MyParseV1.js
+++ MyParseV1.js
@@ -43,7 +43,11 @@ var query = connection.query('INSERT INTO portals (name, lat, lng) VALUES (1, 2,
 
 function Parseando(html){
 //var html='<div class="plext"><!-- false --><div class="pl_timestamp"><div class="pl_timestamp_container"><div class="pl_timestamp_date">2:30PM</div><div class="pl_timestamp_spacer"></div></div></div><div class="pl_content pl_broad"><span class="ENLIGHTENED pl_nudge_player" data-playerstr="@pepitaIluminada" data-isfaction="false">pepitaIluminada</span> linked <span class="pl_portal_name" data-plat="4.591753" data-plng="-74.12433">Piramide De Chichen Itza</span> <span class="pl_portal_address">(Calle 38AS # 34B-1 a 34B-99, Centro Mayor, Chevignon Centro Mayor, Bogotá, Cundinamarca, Colombia)</span> to <span class="pl_portal_name" data-plat="4.592607" data-plng="-74.124022">Colgante Tairona</span> <span class="pl_portal_address">(Calle 38AS # 34D-1 a 34D-33, Centro Mayor, Chevignon Centro Mayor, Bogotá, Cundinamarca, Colombia)</span></div></div>'
-//var html='<div class="plext"><!-- false --><div class="pl_timestamp"><div class="pl_timestamp_container"><div class="pl_timestamp_date">10:53AM</div><div class="pl_timestamp_spacer"></div></div></div><div class="pl_content pl_broad"><span class="ENLIGHTENED pl_nudge_player" data-playerstr="@dcrodg" data-isfaction="false">dcrodg</span> captured <span class="pl_portal_name" data-plat="4.656798" data-plng="-74.054469">Arbol De Helm</span> <span class="pl_portal_address">(Carrera 7 # 73-1 a 73-99, Bogotá, Cundinamarca, Colombia)</span></div></div>';
+//var html='<div class="plext"><!-- false --><div class="pl_timestamp">
+//<div class="pl_timestamp_container"><div class="pl_timestamp_date">10:53AM</div>
+//<div class="pl_timestamp_spacer"></div></div></div>
+//<div class="pl_content pl_broad">
+//<span class="ENLIGHTENED pl_nudge_player" data-playerstr="@dcrodg" data-isfaction="false">dcrodg</span> captured <span class="pl_portal_name" data-plat="4.656798" data-plng="-74.054469">Arbol De Helm</span> <span class="pl_portal_address">(Carrera 7 # 73-1 a 73-99, Bogotá, Cundinamarca, Colombia)</span></div></div>';
 enl="";
 res="";
 var promise = htmlToJson.parse(html, { 
@@ -77,7 +81,7 @@ var promise = htmlToJson.parse(html, {
   }],
   'txt': [".pl_nudge_player", function ($spa) {
      //txt=$spa.text();
-     txt=$spa.find('').text();
+     txt=$spa.text();
     return txt;
   }],
   'action': [".pl_content", function ($spa) {
