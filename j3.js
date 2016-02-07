8f9bbb24fe9df9b10a8fb5238f071cec4d6cf8a7
wposada <wilsonposada@msn.com>
2016-02-06 23:24:29 -0500

    Create j3.js

---
+++ j3.js
@@ -0,0 +1,22 @@
+function walk(currentDirPath, callback) {
+    var fs = require('fs'),
+        path = require('path');
+    fs.readdir(currentDirPath, function (err, files) {
+        if (err) {
+            throw new Error(err);
+        }
+        files.forEach(function (name) {
+            var filePath = path.join(currentDirPath, name);
+            var stat = fs.statSync(filePath);
+            if (stat.isFile()) {
+                callback(filePath, stat);
+            } else if (stat.isDirectory()) {
+                walk(filePath, callback);
+            }
+        });
+    });
+}
+
+walk('path/to/root/dir', function(filePath, stat) {
+    console.log(filePath);
+});
