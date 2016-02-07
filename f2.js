b920887f202d0ec9738e19f49a8d1974d0ac4505
wposada <wilsonposada@msn.com>
2016-02-06 23:12:54 -0500

    Update f2.js

--- f2.js
+++ f2.js
@@ -1,4 +1,8 @@
-files.forEach(function (v,k){
-  // require routes
-  require('./routes/'+v);
-});
+var fs = require('fs');
+var dir = '/media/sf_Ingress/Order/Julio'; // your directory
+
+var files = fs.readdirSync(dir);
+files.sort(function(a, b) {
+               return fs.statSync(dir + a).mtime.getTime() - 
+                      fs.statSync(dir + b).mtime.getTime();
+           });
