3680a953116d25f5ad2aede8fe3f11c9cb913958
wposada <wilsonposada@msn.com>
2016-02-06 23:17:46 -0500

    Update f2.js

--- f2.js
+++ f2.js
@@ -1,9 +1,18 @@
 var fs = require('fs');
 var dir = '/media/sf_Ingress/Order/Julio/'; // your directory
 
-var files = fs.readdirSync(dir);
-files.sort(function(a, b) {
-  console.log(fs.mtime);
-               return fs.statSync(dir + a).mtime.getTime() - 
-                      fs.statSync(dir + b).mtime.getTime();
-           });
+function getFiles (dir, files_){
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files){
+        var name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory()){
+            getFiles(name, files_);
+        } else {
+            files_.push(name);
+        }
+    }
+    return files_;
+}
+
+console.log(getFiles(dir))
