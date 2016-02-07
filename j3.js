e8e9a85c345a7cdc9dc62acd8af61df7d619c278
wposada <wilsonposada@msn.com>
2016-02-06 23:29:21 -0500

    Update j3.js

--- j3.js
+++ j3.js
@@ -19,4 +19,23 @@ function walk(currentDirPath, callback) {
 
 walk('/media/sf_Ingress/Order/Julio/', function(filePath, stat) {
     console.log(filePath);
+    fs.stat(filePath, function(err, stats){
+  if(err){
+    console.error(err);
+  }
+  else{
+    //comprobamos si es un directorio
+    console.log("Es un directorio? =>", stats.isDirectory());
+    //comprobamos si es un fichero
+    console.log("Es un fichero? =>", stats.isFile());
+    //obtenemos el tamaño en bytes del fichero
+    console.log("Tamaño fichero =>", stats.size);
+    //comprobamos si el fichero es un enlace simbolico
+    console.log("Es un enlace simbolico? =>", stats.isSymbolicLink());
+    //imprimimos toda la información del objeto stats
+    console.dir(stats);
+  }
+});
+    
+    
 });
